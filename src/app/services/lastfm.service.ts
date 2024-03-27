2b53b1c26e17ce5822fe9db60bccd21236ab8586
Fox <creasyfox@gmail.com>
2024-03-27 19:52:23 +0100

    basee error handling

--- src/app/services/lastfm.service.ts
+++ src/app/services/lastfm.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from '../../../env.json';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { TopArtistsResponse, LastFmArtistSearchResults, LastFmArtistDetails, LastFmArtistAlbums } from '../../assets/interfaces/lastfm';
 
 const apiKey = environment.lastfmkey;
@@ -14,33 +15,46 @@ export class LastfmService {
 
 	private baseUrl = 'https://ws.audioscrobbler.com/2.0/';
 
+	private handleError(error: HttpErrorResponse) {
+		let errorMessage = 'An unknown error occurred!';
+		if (error.error) {
+			// LastFm errors
+			errorMessage = `Error Code: ${error.error.error}, Message: ${error.error.message}`;
+		} else {
+			// Backend returned an unsuccessful response code
+			errorMessage = `Error Code: ${error.status}, Message: ${error.message}`;
+		}
+		console.error(errorMessage);
+		return throwError(() => new Error(errorMessage));
+	}
+
 	// Searches for artists on Last.fm matching the searchTerm, limited to a specified number or 3 by default, and returns the results.
 	searchArtists(searchTerm: string, limit: number = 3): Observable<LastFmArtistSearchResults> {
 		const url = `${this.baseUrl}?method=artist.search&artist=${encodeURIComponent(searchTerm)}&api_key=${apiKey}&limit=${limit}&format=json`;
-		return this.http.get<LastFmArtistSearchResults>(url);
+		return this.http.get<LastFmArtistSearchResults>(url).pipe(catchError(this.handleError));
 	}
 
 	// Retrieves detailed information about an artist from Last.fm using the artist's name.
 	getArtistInfo(artistName: string): Observable<any> {
 		const url = `${this.baseUrl}?method=artist.getinfo&artist=${artistName}&api_key=${apiKey}&format=json`;
-		return this.http.get(url);
+		return this.http.get(url).pipe(catchError(this.handleError));
 	}
 
 	// Fetches the top artists for a specified country from Last.fm, limited to a specified number or 10 by default.
 	getTopArtists(country: string, limit: number = 10): Observable<TopArtistsResponse> {
 		const url = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=${apiKey}&limit=${limit}&format=json`;
-		return this.http.get<TopArtistsResponse>(url);
+		return this.http.get<TopArtistsResponse>(url).pipe(catchError(this.handleError));
 	}
 
 	// Gets detailed information about an artist from Last.fm, similar to getArtistInfo but with typed response.
 	getArtistDetails(artistName: string): Observable<LastFmArtistDetails> {
 		const url = `http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artistName}&api_key=${apiKey}&format=json`;
-		return this.http.get<LastFmArtistDetails>(url);
+		return this.http.get<LastFmArtistDetails>(url).pipe(catchError(this.handleError));
 	}
 
 	// Retrieves the top albums for an artist from Last.fm, limited to a specified number or 10 by default.
 	getArtistAlbums(artistName: string, limit: number = 10): Observable<LastFmArtistAlbums> {
 		const url = `${this.baseUrl}?method=artist.gettopalbums&artist=${encodeURIComponent(artistName)}&api_key=${apiKey}&limit=${limit}&format=json`;
-		return this.http.get<LastFmArtistAlbums>(url);
+		return this.http.get<LastFmArtistAlbums>(url).pipe(catchError(this.handleError));
 	}
 }
